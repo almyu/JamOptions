1348035682f69df80fa15e171b56aeeb3f28b38f
almyu <hire@indeed.li>
2015-08-19 18:37:13 +0700

    Btw, save

--- Audio/Scripts/AudioSliders.cs
+++ Audio/Scripts/AudioSliders.cs
@@ -8,65 +8,38 @@ namespace JamOptions.Audio {
     public class AudioSliders : MonoBehaviour {
 
         public RectTransform template;
+        public AudioPrefs prefs;
 
-        [System.Serializable]
-        public struct MixerSetup {
-            public AudioMixer mixer;
-
-            [System.Serializable]
-            public struct Parameter {
-                public string label, name;
-                public Vector2 range;
-            }
-
-            public Parameter[] parameters;
-        }
-
-        public MixerSetup[] mixers;
-
-
-        private void OnValidate() {
-            if (mixers == null) return; // Wait for initial deserialization
-            if (mixers.Length != 0) return;
-
-            mixers = System.Array.ConvertAll(Resources.FindObjectsOfTypeAll<AudioMixer>(), mixer => {
-                return new MixerSetup {
-                    mixer = mixer,
-                    parameters = new MixerSetup.Parameter[] {
-                        new MixerSetup.Parameter { label = "Volume", name = "Volume", range = new Vector2(-80, 20) }
-                    }
-                };
-            });
-        }
 
         private void OnEnable() {
+            prefs = prefs.GetInstance();
+
             var itemIndex = 0;
 
-            foreach (var setup in mixers) {
-                foreach (var param in setup.parameters) {
-                    ++itemIndex;
+            foreach (var param in prefs.keptParameters) {
+                ++itemIndex;
 
-                    var xf = Instantiate(template);
-                    xf.SetParent(transform, false);
-                    xf.pivot = new Vector2(xf.pivot.x, itemIndex);
-                    xf.gameObject.SetActive(true);
+                var xf = Instantiate(template);
+                xf.SetParent(transform, false);
+                xf.pivot = new Vector2(xf.pivot.x, itemIndex);
+                xf.gameObject.SetActive(true);
 
-                    var label = xf.GetComponentInChildren<Text>();
-                    if (label)
-                        label.text = param.label;
+                var label = xf.GetComponentInChildren<Text>();
+                if (label)
+                    label.text = param.label;
 
-                    var slider = xf.GetComponentInChildren<Slider>();
-                    if (slider) {
-                        var mixer = setup.mixer;
-                        var paramName = param.name;
+                var slider = xf.GetComponentInChildren<Slider>();
+                if (slider) {
+                    var localParam = param;
 
-                        slider.onValueChanged.AddListener(level => {
-                            mixer.SetFloat(paramName, level);
-                        });
+                    slider.onValueChanged.AddListener(value => {
+                        localParam.value = value;
+                    });
 
-                        slider.minValue = param.range[0];
-                        slider.maxValue = param.range[1];
-                    }
+                    slider.minValue = param.range[0];
+                    slider.maxValue = param.range[1];
+
+                    slider.value = param.value;
                 }
             }
 
